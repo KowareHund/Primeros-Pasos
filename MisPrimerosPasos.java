a3f8763c9c8693d3f80f3d355dc3391762d0d548
KowareHund <jricardom@unal.edu.co>
2025-04-14 15:50:44 -0500

    Add files via upload

---
+++ MisPrimerosPasos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mis.primeros.pasos;
+
+/**
+ *
+ * @author Esteban Ricardo
+ */
+public class MisPrimerosPasos {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, ya se imprimir frases");
+    }
+}
+
+    
+
